ac4efa0a78f6192c412e645400545dc62b8db5d3
SoheibBettahar <soheibbett@gmail.com>
2023-10-15 21:32:11 +0100

    Create User model

--- app/src/main/java/com/soheibbettahar/yobi_test/data/model/User.kt
+++ app/src/main/java/com/soheibbettahar/yobi_test/data/model/User.kt
@@ -1,14 +1,14 @@
 package com.soheibbettahar.yobi_test.data.model
 
 data class User(
-    val id: String,
-    val title: String,
-    val firstName: String,
-    val lastName: String,
-    val picture: String
+    val id: String = "",
+    val title: String = "",
+    val firstName: String = "",
+    val lastName: String = "",
+    val picture: String = ""
 ) {
     val fullName: String
-        get() = when{
+        get() = when {
             title.isEmpty() -> "$firstName $lastName"
             firstName.isEmpty() -> "$title.$lastName"
             else -> "$title.$firstName $lastName"
