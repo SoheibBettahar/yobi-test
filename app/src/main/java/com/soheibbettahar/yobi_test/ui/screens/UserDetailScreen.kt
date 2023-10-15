71ff6d51793ae60c93c9ecc1cdb16b29d368dc76
SoheibBettahar <soheibbett@gmail.com>
2023-10-15 20:41:36 +0100

    Create Skeleton composables for UsersScreen and UserDetailScreen

---
+++ app/src/main/java/com/soheibbettahar/yobi_test/ui/screens/UserDetailScreen.kt
@@ -0,0 +1,17 @@
+package com.soheibbettahar.yobi_test.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun UserDetailScreen() {
+    Text(text = "UserDetail Screen")
+}
+
+
+@Composable
+@Preview(showBackground = true)
+fun UserDetailScreenPreview(){
+    UserDetailScreen()
+}
---
+++ app/src/main/java/com/soheibbettahar/yobi_test/ui/screens/UsersScreen.kt
@@ -0,0 +1,17 @@
+package com.soheibbettahar.yobi_test.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun UsersScreen() {
+    Text(text = "Users Screen")
+}
+
+
+@Composable
+@Preview(showBackground = true)
+fun UsersScreenPreview() {
+    UsersScreen()
+}
