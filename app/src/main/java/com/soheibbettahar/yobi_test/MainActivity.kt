350e020abe1a4e8e450d7b827eb448c973acdeeb
SoheibBettahar <soheibbett@gmail.com>
2023-10-15 20:42:32 +0100

    Delete Greeting composable

--- app/src/main/java/com/soheibbettahar/yobi_test/MainActivity.kt
+++ app/src/main/java/com/soheibbettahar/yobi_test/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.soheibbettahar.yobi_test.ui.screens.UsersScreen
 import com.soheibbettahar.yobi_test.ui.theme.YobitestTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +20,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    UsersScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    YobitestTheme {
-        Greeting("Android")
     }
 }
