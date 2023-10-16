09544f5c0daddd4954cfefcc483f01290b43a0a6
SoheibBettahar <soheibbett@gmail.com>
2023-10-16 23:08:36 +0100

    create UserDetailScreenNavigation.kt

---
+++ app/src/main/java/com/soheibbettahar/yobi_test/ui/navigation/UserDetailScreenNavigation.kt
@@ -0,0 +1,28 @@
+package com.soheibbettahar.yobi_test.ui.navigation
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.soheibbettahar.yobi_test.ui.screens.UserDetailScreen
+
+const val UserDetailRoute = "UserDetail"
+
+private const val USER_ID_ARG = "userId"
+
+fun NavGraphBuilder.userDetailScreen() {
+
+    composable(
+        route = "$UserDetailRoute/{$USER_ID_ARG}",
+        arguments = listOf(navArgument(USER_ID_ARG) { type = NavType.StringType })
+    ) {
+        UserDetailScreen()
+    }
+
+}
+
+
+internal class UserDetailArgs private constructor(val userId: String) {
+    constructor(savedStateHandle: SavedStateHandle) : this((checkNotNull(savedStateHandle[USER_ID_ARG]) as String))
+}
