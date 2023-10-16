ea37952ad43e0f9be11abe8409f4ceefdafc5b7a
SoheibBettahar <soheibbett@gmail.com>
2023-10-17 00:42:49 +0100

    Create UserDetailViewModel

---
+++ app/src/main/java/com/soheibbettahar/yobi_test/ui/viewmodels/UserDetailViewModel.kt
@@ -0,0 +1,54 @@
+package com.soheibbettahar.yobi_test.ui.viewmodels
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.soheibbettahar.yobi_test.data.model.UserDetail
+import com.soheibbettahar.yobi_test.data.repository.UserRepository
+import com.soheibbettahar.yobi_test.ui.navigation.UserDetailArgs
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+sealed interface UserDetailUiState {
+    data class Success(val data: UserDetail) : UserDetailUiState
+    data class Error(val exception: Throwable? = null) : UserDetailUiState
+    object Loading : UserDetailUiState
+}
+
+@HiltViewModel
+class UserDetailViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    private val userRepository: UserRepository
+) : ViewModel() {
+
+    private val args = UserDetailArgs(savedStateHandle)
+    private val userId = args.userId
+
+    init {
+        fetchUserDetail(userId)
+    }
+
+    private val _userDetailUiState = MutableStateFlow<UserDetailUiState>(UserDetailUiState.Loading)
+    val userDetailUiState = _userDetailUiState.asStateFlow()
+
+
+    fun fetchUserDetail(userId: String) {
+        viewModelScope.launch {
+
+            userRepository
+                .fetchUserDetail(userId = userId)
+                .map { userDetail -> UserDetailUiState.Success(userDetail) }
+                .onStart { _userDetailUiState.value = UserDetailUiState.Loading }
+                .catch { throwable ->
+                    _userDetailUiState.value = UserDetailUiState.Error(throwable)
+                }
+        }
+    }
+
+}
