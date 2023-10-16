c09e3bce0a96754b15cb1dffc9602c729ac6fa0d
SoheibBettahar <soheibbett@gmail.com>
2023-10-16 10:16:30 +0100

    Create NetworkModule

---
+++ app/src/main/java/com/soheibbettahar/yobi_test/di/NetworkModule.kt
@@ -0,0 +1,63 @@
+package com.soheibbettahar.yobi_test.di
+
+import com.soheibbettahar.yobi_test.data.network.UserService
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object NetworkModule {
+
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        //TODO: remove loggingInterceptor later
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    fun provideMoshiInstance(): Moshi {
+        return Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitInstance(okHttpClient: OkHttpClient, moshi: Moshi): Retrofit {
+
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl("https://dummyapi.io/data/v1")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideUserServiceInstance(retrofit: Retrofit): UserService {
+        return retrofit.create(UserService::class.java)
+    }
+
+
+}
