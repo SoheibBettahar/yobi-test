77fb986318edfed7d567a12f3d12008dd3e58cd8
SoheibBettahar <soheibbett@gmail.com>
2023-10-17 00:37:37 +0100

    in UserRepository create fetchUserDetail function

--- app/src/main/java/com/soheibbettahar/yobi_test/data/repository/UserRepository.kt
+++ app/src/main/java/com/soheibbettahar/yobi_test/data/repository/UserRepository.kt
@@ -2,10 +2,13 @@ package com.soheibbettahar.yobi_test.data.repository
 
 import androidx.paging.PagingData
 import com.soheibbettahar.yobi_test.data.model.User
+import com.soheibbettahar.yobi_test.data.model.UserDetail
 import kotlinx.coroutines.flow.Flow
 
 interface UserRepository {
 
     fun fetchUsers(): Flow<PagingData<User>>
+
+    fun fetchUserDetail(userId: String): Flow<UserDetail>
 
 }
--- app/src/main/java/com/soheibbettahar/yobi_test/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/soheibbettahar/yobi_test/data/repository/UserRepositoryImpl.kt
@@ -6,9 +6,13 @@ import androidx.paging.PagingData
 import com.soheibbettahar.yobi_test.data.PAGE_SIZE
 import com.soheibbettahar.yobi_test.data.UsersPagingSource
 import com.soheibbettahar.yobi_test.data.model.User
+import com.soheibbettahar.yobi_test.data.model.UserDetail
 import com.soheibbettahar.yobi_test.data.network.UserService
+import com.soheibbettahar.yobi_test.data.network.toExternalModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
@@ -24,5 +28,10 @@ class UserRepositoryImpl @Inject constructor(
             UsersPagingSource(userService)
         }
     ).flow
+
+    override fun fetchUserDetail(userId: String): Flow<UserDetail> = flow {
+        val userDetail = userService.fetchUserDetail(userId).toExternalModel()
+        emit(userDetail)
+    }.flowOn(dispatchIo)
 
 }
