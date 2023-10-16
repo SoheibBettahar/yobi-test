409e0cbc7394cec607ee043a8ccf30f0e5b14fc2
SoheibBettahar <soheibbett@gmail.com>
2023-10-16 10:15:53 +0100

    Create YobitestApplication

---
+++ app/src/main/java/com/soheibbettahar/yobi_test/YobitestApplication.kt
@@ -0,0 +1,9 @@
+package com.soheibbettahar.yobi_test
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Singleton
+
+@Singleton
+@HiltAndroidApp
+class YobitestApplication: Application()
