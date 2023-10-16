08756bf1d451f4d0a6760fb1ef0d7a503989d038
SoheibBettahar <soheibbett@gmail.com>
2023-10-17 00:37:16 +0100

    in UserService create fetchUserDetail function

--- app/src/main/java/com/soheibbettahar/yobi_test/data/network/UserService.kt
+++ app/src/main/java/com/soheibbettahar/yobi_test/data/network/UserService.kt
@@ -1,7 +1,7 @@
 package com.soheibbettahar.yobi_test.data.network
 
 import retrofit2.http.GET
-import retrofit2.http.Headers
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 
@@ -12,5 +12,11 @@ interface UserService {
         @Query("page") page: Int,
         @Query("limit") limit: Int
     ): NetworkUsersPage
+
+    @GET("user/{id}")
+    suspend fun fetchUserDetail(
+        @Path("id") id: String,
+    ): NetworkUserDetail
+
 
 }
