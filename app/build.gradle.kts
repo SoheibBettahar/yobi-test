c33d95ec5b9a9dd8a9859011d1f2a0a5f7d7e13f
SoheibBettahar <soheibbett@gmail.com>
2023-10-17 12:22:59 +0100

    add extended material icons dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation ("androidx.compose.material:material-icons-extended")
 
     //hilt
     implementation ("com.google.dagger:hilt-android:2.44")
