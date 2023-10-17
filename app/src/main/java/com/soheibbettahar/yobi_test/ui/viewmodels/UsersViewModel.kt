fc675d6ec389df3de325b967198b5db6e2336eb8
SoheibBettahar <soheibbett@gmail.com>
2023-10-17 10:21:07 +0100

    in UsersViewModel add searchTerm stateHolder and use it to fetchUsers onChange
    - add searchTerm state holder
    - add onSearchTermChanged funtion
    - add searchTermFlow
    - update usersPagingDataFlow when searchTerm changes

--- app/src/main/java/com/soheibbettahar/yobi_test/ui/viewmodels/UsersViewModel.kt
+++ app/src/main/java/com/soheibbettahar/yobi_test/ui/viewmodels/UsersViewModel.kt
@@ -1,5 +1,9 @@
 package com.soheibbettahar.yobi_test.ui.viewmodels
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -8,17 +12,42 @@ import androidx.paging.cachedIn
 import com.soheibbettahar.yobi_test.data.model.User
 import com.soheibbettahar.yobi_test.data.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @HiltViewModel
 class UsersViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
     private val usersRepository: UserRepository
 ) : ViewModel() {
 
-    val usersPagingDataFlow: Flow<PagingData<User>> =
-        usersRepository.fetchUsers().cachedIn(viewModelScope)
+    var searchTerm by mutableStateOf("")
+        private set
+
+    @OptIn(FlowPreview::class)
+    private val searchTermFlow: Flow<String> = snapshotFlow { searchTerm }
+        .debounce(500)
+        .map { it.trim() }
+        .distinctUntilChanged()
+        .conflate()
+
+    val usersPagingDataFlow: Flow<PagingData<User>> = searchTermFlow.flatMapLatest {
+        usersRepository.fetchUsers(it)
+    }.distinctUntilChanged()
+        .cachedIn(viewModelScope)
+
+
+    fun onSearchTermChanged(text: String) {
+        searchTerm = text
+    }
 
 }
