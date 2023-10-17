34aa885459e68d44f28301cab1cf99b6cd849cc6
SoheibBettahar <soheibbett@gmail.com>
2023-10-17 02:10:44 +0100

    Fix bug in UserDetailViewModel (declare data holder before the init block)

--- app/src/main/java/com/soheibbettahar/yobi_test/ui/viewmodels/UserDetailViewModel.kt
+++ app/src/main/java/com/soheibbettahar/yobi_test/ui/viewmodels/UserDetailViewModel.kt
@@ -10,9 +10,9 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 sealed interface UserDetailUiState {
@@ -30,25 +30,22 @@ class UserDetailViewModel @Inject constructor(
     private val args = UserDetailArgs(savedStateHandle)
     private val userId = args.userId
 
+    private val _userDetailUiState = MutableStateFlow<UserDetailUiState>(UserDetailUiState.Loading)
+    val userDetailUiState = _userDetailUiState.asStateFlow()
+
     init {
         fetchUserDetail(userId)
     }
 
-    private val _userDetailUiState = MutableStateFlow<UserDetailUiState>(UserDetailUiState.Loading)
-    val userDetailUiState = _userDetailUiState.asStateFlow()
-
 
     fun fetchUserDetail(userId: String) {
-        viewModelScope.launch {
-
             userRepository
                 .fetchUserDetail(userId = userId)
-                .map { userDetail -> UserDetailUiState.Success(userDetail) }
+                .map { userDetail -> _userDetailUiState.value = UserDetailUiState.Success(userDetail) }
                 .onStart { _userDetailUiState.value = UserDetailUiState.Loading }
                 .catch { throwable ->
                     _userDetailUiState.value = UserDetailUiState.Error(throwable)
-                }
-        }
+                }.launchIn(viewModelScope)
     }
 
 }
